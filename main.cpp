f5061734856e29fb07aaf2cb960cb5135c94f6f1
AgustinPessina00 <apessina@fi.uba.ar>
2023-11-12 20:47:12 +0000

    Modifico main para dejarlo limpio

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 //=====[Libraries]=============================================================
 
-#include "smart_home_system.h"
-
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
