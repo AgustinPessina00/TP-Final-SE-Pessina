33574c6d44ce232b1933075c08236dab18faefc4
AgustinPessina00 <apessina@fi.uba.ar>
2023-11-21 18:31:10 +0000

    Prueba

--- modules/TrackerGPS/TrackerGPS.cpp
+++ modules/TrackerGPS/TrackerGPS.cpp
@@ -68,7 +68,7 @@ void trackerGPS::positionUpdate ( char * str)
     }
     readyToReadNewGeo = true;
 //  timeToSend = false;
-    } 
+//  } 
 }
 
 //=====[Implementations of private functions]==================================
